b0d48f17d7d05ba05cb79434dd99fe08d172759f
Andrew Malikov <andrewmalikov@sent.com>
2023-11-16 09:16:15 +0000

    Restore logs commands

---
+++ src/commands/logs/arrange.ts
@@ -0,0 +1,64 @@
+import {
+  listActiveContainers,
+  listenContainerLogs,
+} from "../../docker/docker-wrapper";
+import { EmptyResult, Failure } from "../../result/result";
+import { GetStorage, StorageDefinition } from "../../storage/storage";
+
+export type ArrangeContainersLogsRequest = {
+  containerFilter: string;
+  storageDefinition: StorageDefinition;
+};
+
+export async function arrangeContainersLogs(
+  getStorage: GetStorage,
+  { containerFilter, storageDefinition }: ArrangeContainersLogsRequest
+): Promise<EmptyResult> {
+  const storageResult = await getStorage(storageDefinition);
+  if (storageResult instanceof Failure) {
+    return storageResult.asEmpty();
+  }
+  const storage = storageResult.asOk();
+
+  if (!(await storage.isHealthy())) {
+    const storageMetadata = storage.getStorageMetadata();
+    return EmptyResult.ofFailure(
+      `The storage ${storageMetadata.toString()} is unhealthy.`
+    );
+  }
+
+  const containersResult = await listActiveContainers(containerFilter);
+  if (containersResult instanceof Failure) {
+    return containersResult.asEmpty();
+  }
+  const containers = containersResult.asOk();
+
+  console.log("Found containers by filter", containerFilter);
+  console.table(containers);
+
+  if (containers.length === 0) {
+    return EmptyResult.ofOk();
+  }
+
+  for (const container of containers) {
+    console.log("Reading container", container.id, "logs");
+    const containerLogsResult = listenContainerLogs(container.id);
+    if (containerLogsResult instanceof Failure) {
+      return containerLogsResult.asEmpty();
+    }
+    const containerLogs = containerLogsResult.asOk();
+
+    storage.saveLogs(container.id, containerLogs).then((saveResult) => {
+      if (saveResult instanceof Failure) {
+        console.error(
+          "Failed to start saving logs of container",
+          container.id,
+          saveResult.message,
+          saveResult.error
+        );
+      }
+    });
+  }
+
+  return EmptyResult.ofOk();
+}
---
+++ src/commands/logs/list.ts
@@ -0,0 +1,33 @@
+import { EmptyResult, Failure } from "../../result/result";
+import { GetStorage, StorageDefinition } from "../../storage/storage";
+
+export type ListContainersRequest = {
+  storageDefinition: StorageDefinition;
+};
+
+export async function listContainers(
+  getStorage: GetStorage,
+  { storageDefinition }: ListContainersRequest
+): Promise<EmptyResult> {
+  const storageResult = await getStorage(storageDefinition);
+  if (storageResult instanceof Failure) {
+    return storageResult.asEmpty();
+  }
+  const storage = storageResult.asOk();
+
+  if (!(await storage.isHealthy())) {
+    const storageMetadata = storage.getStorageMetadata();
+    return EmptyResult.ofFailure(
+      `The storage ${storageMetadata.toString()} is unhealthy.`
+    );
+  }
+
+  const containersResult = await storage.getLoggedContainers();
+  if (containersResult instanceof Failure) {
+    return containersResult.asEmpty();
+  }
+
+  console.log("Found containers' ids", containersResult.asOk());
+
+  return EmptyResult.ofOk();
+}
